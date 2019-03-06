ffc17376e6e1c97540e7479d3230bb9dfc4af5ce
Shubham Dhage <shubhamdhage930@gmail.com>
2019-03-06 18:20:42 +0530

    Updates

---
+++ precache-manifest.c2612cd74d72cda39143dfec9f66b562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4a527ca6330ebf7e247",
+    "url": "/react-redux/static/js/runtime~main.e4a527ca.js"
+  },
+  {
+    "revision": "213cb0bf2538838239ad",
+    "url": "/react-redux/static/js/main.213cb0bf.chunk.js"
+  },
+  {
+    "revision": "7533c692f903f38e523d",
+    "url": "/react-redux/static/js/2.7533c692.chunk.js"
+  },
+  {
+    "revision": "213cb0bf2538838239ad",
+    "url": "/react-redux/static/css/main.837493de.chunk.css"
+  },
+  {
+    "revision": "7533c692f903f38e523d",
+    "url": "/react-redux/static/css/2.40db5bd6.chunk.css"
+  },
+  {
+    "revision": "dc7f03b6b981a3b2aafa5d5be52a4cb5",
+    "url": "/react-redux/index.html"
+  }
+];
